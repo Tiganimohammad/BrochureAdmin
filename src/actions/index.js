7ada6118415533e6194392894ace09c9ee9be3b9
tigani <tiganimoh@gmail.com>
2019-04-04 16:21:12 +0200

    showuld work

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 
   
+let axiosConfig = {
+    headers: {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+    }
+  };
+  
+
 export function loginUser(email,password){
-    const req = axios.post('http://89.163.221.56:8881/api/company/login',{email,password})
-           .then(Response => Response.data)
+    const req = axios.post('http://89.163.221.56:8881/api/company/login',{email,password},
+    axiosConfig)
+           .then(Response => console.log(Response))
 
     return{
         type:'USER_LOGIN',
@@ -11,22 +20,22 @@ export function loginUser(email,password){
     }
 }    
   
-// export function getProductsList(){
-//     const request = axios.get('http://localhost:3003/Productlist')
-//     .then(response => response.data);
-//     return {
-//     type:'GET_ProductLists',
-//     payload:request
-// }
-// }
+export function getProductsList(){
+    const request = axios.get('http://localhost:3003/Productlist')
+    .then(response => response.data);
+    return {
+    type:'GET_ProductLists',
+    payload:request
+}
+}
 
-// export function getMyProfile(){
-//     const request = axios.get('http://localhost:3003/MyProfile')
-//     .then(response => response.data);
-//     return {
-//     type:'GET_MyProfile',
-//     payload:request
-// }
-//}
+export function getMyProfile(){
+    const request = axios.get('http://localhost:3003/MyProfile')
+    .then(response => response.data);
+    return {
+    type:'GET_MyProfile',
+    payload:request
+}
+}
 
 
